ec8cc2935e728b46d47dc51bf50508ea98b6ee19
Thenuka02 <kthenuka02@gmail.com>
2021-11-29 18:55:23 +0530

    WeatherApplicatin

---
+++ src/components/frontpage.js
@@ -0,0 +1,22 @@
+// import React from "react";
+// const frontpage = props => {
+//     return(
+// <section class="top-banner">
+//   <div class="container">
+//     <h1 class="heading">Simple Weather App</h1>
+//     <form>
+//       <input type="text" placeholder="Search for a city" autofocus>
+//       <button type="submit">SUBMIT</button>
+//       <span class="msg"></span>
+//     </form>
+//   </div>
+// </section>
+// <section class="ajax-section">
+//   <div class="container">
+//     <ul class="cities"></ul>
+//   </div>
+// </section>
+// );
+// };
+
+// export default frontpage;
